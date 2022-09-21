e7e7563d6bc184c9c168cf81bea5bcf17b36898c
folashade1024 <fawohunreomobolanle@gmail.com>
2022-09-21 16:00:19 +0100

    corrected file

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
 console.log('Hello, World!')
+
+let Name="folashade"
+function myName(){
+    alert(Name)
+}
+myName()
+function addNumber(a,b){\
+a+b
+
+console.log(`The sum of two number is ${a+b}` )
+}
+
+addNumber(2,3)
+addNumber(10,5)
+addNumber(20,3)
